0a7fd4f8dcc00a464f4fc2d2bc802928fa74ca8f
Michael Lohier <lohiermichael@gmail.com>
2019-11-07 10:46:37 +0700

    Wrap up first version of guessing game

---
+++ section_11:javasript_basics:control_flow/2-guessing_game/game.js
@@ -0,0 +1,20 @@
+// Create secret number
+var secretNumber = 4;
+
+// Ask user for guess
+var stringGuess = prompt("Guess the number");
+var guess = Number(stringGuess);
+
+// Check if guess right
+if (guess === secretNumber) {
+    alert("YOU GOT IT RIGHT")
+}
+// Otherwise check if higher
+else if (guess > secretNumber){
+    alert("Too high! Guess again!")
+}
+
+// Otherwise check if lower
+else {
+    alert("Too low! Guess again!")
+}
