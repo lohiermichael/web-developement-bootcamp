63dc4d87d5622a3900ec4b6ac9c2ea921a5d53b4
Michael Lohier <lohiermichael@gmail.com>
2019-12-10 15:41:39 +0700

    Discover the notion of templates in EJS

---
+++ section_26:intermediate_express/1-more_express/ejs_demo/app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+
+
+
+app.get("/", function(req, res){
+    // We can either write some HTML here but it is really painful
+    // res.send("<h1>Welcome to the home page!<\h1><h2>blabla<\h2>");
+    // Or we can render a file by using the render method on the res object
+    // We need to create the file in a separate directory
+    res.render("home.ejs")
+});
+
+app.get("/fallinlovewith/:thing", function(req, res){
+    var thing = req.params.thing;
+    // res.send("You fell in love with " + thing);
+
+    // We can give arguments in the render method as parameters that will be used in the ejs file
+    res.render("love.ejs", {thingVar: thing});
+});
+
+
+app.listen(3000, function() {
+    console.log("More express on port 3000");
+  });
---
+++ section_26:intermediate_express/1-more_express/readme.md
@@ -0,0 +1,13 @@
+# Rendering HTML and Templates
+
+* Use res.render() to render HTML (from on EJS file)
+
+This allows us to make reference to an HTML page whenever we want to access a specif route
+
+* Explain what EJS is and why we use it
+
+EJS allows us to include Javascript code within HTML
+
+* Pass variables to EJS templates
+
+The way we write Javascript code in EJS file is by passing variables within our HTML files. This makes the content dynamic
