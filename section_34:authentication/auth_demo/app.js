1a4b06a4d7bf95b380adb80bdcbbe92e627e08b9
Michael Lohier <lohiermichael@gmail.com>
2019-12-31 19:18:07 +0700

    At this state it is not working

--- section_34:authentication/auth_demo/app.js
+++ section_34:authentication/auth_demo/app.js
@@ -1,39 +1,42 @@
 // Import Express
-const   express               = require('express'),
-        mongoose              = require('mongoose'),
-        passport              = require('passport'),
-        bodyParser            = require('body-parser'),
-        User                  = require("./models/user"),  
-        LocalStrategy         = require('passport-local'),
-        passportLocalMongoose = require('passport-local-mongoose'),
-        expressSession        = require("express-session"),
-        app                   = express();
-
+const express = require("express"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  bodyParser = require("body-parser"),
+  User = require("./models/user"),
+  LocalStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose"),
+  expressSession = require("express-session");
 
 // Connect to the database
 mongoose.connect(
-"mongodb://localhost/auth_demo_app",
-{
+  "mongodb://localhost/auth_demo_app",
+  {
     useUnifiedTopology: true,
     useNewUrlParser: true
-},
-err => {
+  },
+  err => {
     if (err) console.error(err);
     else console.log("Connected to the mongodb");
-}
+  }
 );
+mongoose.set("useCreateIndex", true);
 
-app.use(expressSession({
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  expressSession({
     secret: "This is the auth app",
     resave: false,
     saveUninitialized: false
-}));
-mongoose.set('useCreateIndex', true);
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Add th authentication to the local strategy to log in
+passport.use(new LocalStrategy(User.authenticate()));
 // Take data from the session and encode it
 passport.serializeUser(User.serializeUser());
 // Take data from the session and decode it
@@ -43,13 +46,12 @@ passport.deserializeUser(User.deserializeUser());
 // ROUTES
 // ==========
 
-
-app.get("/", (req,res) => {
-    res.render("home");
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get("/secret", (req,res) => {
-    res.render("secret");
+app.get("/secret", (req, res) => {
+  res.render("secret");
 });
 
 // ==========
@@ -57,30 +59,49 @@ app.get("/secret", (req,res) => {
 // ==========
 
 // Sign up to the form
-app.get("/register", (req,res) => {
-    res.render("register");
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
 // Handling user sign up
-app.post("/register", (req,res) => {
-    // The following lines take a user with its name only
-    // Separately, its password
-    // It will handle a new user creating a hash to store the password in the database
-    User.register(new User({ username: req.body.userName}), req.body.password, (err, user) => {
-        if (err) {
-            console.log("Err: ", err);
-            return res.render("register");
-        }
-        // Log the user in
-        passport.authenticate("local")(res, req, function(){
-            console.log(user);
-            res.redirect("/secret");
-        });
-    });
-
+app.post("/register", (req, res) => {
+  // The following lines take a user with its name only
+  // Separately, its password
+  // It will handle a new user creating a hash to store the password in the database
+  User.register(
+    new User({ username: req.body.userName }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        return res.render("register");
+      }
+      // Log the user in
+      passport.authenticate("local")(res, req, function() {
+        console.log(user);
+        res.redirect("/secret");
+      });
+    }
+  );
 });
 
+// LOGIN
+
+// Render login form
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// Login logic
+// Middleware: is run before it callback function
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+  })
+);
 
 // Listen on a PORT
 const PORT = process.env.PORT || 3000;
-app.listen (PORT, () => console.log(`Server Running On Port ${PORT}`));
+app.listen(PORT, () => console.log(`Server Running On Port ${PORT}`));
